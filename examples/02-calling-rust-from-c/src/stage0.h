908a7b2b397ec523f83c299752a62f96a76984f8
Tristan Penman <tristan@tristanpenman.com>
2023-04-30 13:11:59 +1000

    Add graphics for example 2

---
+++ examples/02-calling-rust-from-c/src/stage0.h
@@ -0,0 +1,7 @@
+#ifndef __STAGE0_H
+#define __STAGE0_H
+
+void stage0_loop(int pending_gfx);
+void stage0_init();
+
+#endif
---
+++ examples/02-calling-rust-from-c/src/stage1.h
@@ -0,0 +1,7 @@
+#ifndef __STAGE1_H
+#define __STAGE1_H
+
+void stage1_loop(int pending_gfx);
+void stage1_init();
+
+#endif
