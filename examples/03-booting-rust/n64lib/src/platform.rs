0d656f8896ffa70c720fe9144d5e1a6a1f1812ef
Tristan Penman <tristan@tristanpenman.com>
2024-07-06 23:32:51 +1000

    Update example 03 to use nust64

--- examples/03-booting-rust/n64lib/src/platform.rs
+++ examples/03-booting-rust/n64lib/src/platform.rs
@@ -1,8 +1,7 @@
 #[cfg(any(unix, windows, target_vendor = "nintendo64"))]
-pub use crate::math::*;
 
 #[cfg(target_vendor = "nintendo64")]
 use core::arch::global_asm;
 
 #[cfg(target_vendor = "nintendo64")]
-global_asm!(include_str!("platforms/n64/entrypoint.s"));
+global_asm!(include_str!("entrypoint.s"));
